a0a90e107e95f6d9d20127c82bc077e94d556462
Takashi Masuda <masutaka.net@gmail.com>
2013-09-24 21:19:08 +0900

    Add labels

--- app/controllers/ArticleController.rb
+++ app/controllers/ArticleController.rb
@@ -6,5 +6,23 @@ class ArticleController < UIViewController
 
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     self.view.addSubview @table
+
+    @table.dataSource = self
+    @data = ("A".."Z").to_a
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+    cell.textLabel.text = @data[indexPath.row]
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.count
   end
 end
