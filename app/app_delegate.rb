33d42c21715ceb2f086ee7852bd589d976a4eef1
Takashi Masuda <masutaka.net@gmail.com>
2013-11-05 19:57:41 +0900

    Add view
    http://mobiletou.ch/2013/10/rubymotion-qiita%E3%81%AA%E3%82%A2%E3%83%97%E3%83%AA%E3%82%92%E4%BD%9C%E3%82%8D%E3%81%86-2

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    url = 'https://qiita.com/api/v1/tags/RubyMotion/items'
-    BW::HTTP.get(url) do |response|
-      if response.ok?
-        json = BW::JSON.parse(response.body.to_s)
-        json.each do |data|
-          p "======================================="
-          p "タイトル: #{data["title"]}"
-          p "更新日: #{data["updated_at_in_words"]}"
-          p "ユーザ: #{data["user"]["url_name"]}"
-          p "======================================="
-        end
-      else
-        p response.error_message
-      end
-    end
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    # ドリルダウンの遷移を作りたいので EntriesController のインスタンスを作り、UINavigationController の中に入れる
+    entries_controller = EntriesController.new
+    navigation_controller = UINavigationController.alloc.initWithRootViewController(entries_controller)
+    @window.rootViewController = navigation_controller
+    @window.makeKeyAndVisible
     true
   end
 end
