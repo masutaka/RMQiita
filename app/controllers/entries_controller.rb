38960099f2473ddc109a00738d104db36f7d2ca8
Takashi Masuda <masutaka.net@gmail.com>
2013-11-05 20:55:44 +0900

    Refactor using controllers/entry_controller.rb and views/entry_cell.rb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,33 +14,31 @@ class EntriesController < UITableViewController
         p error_message
       end
     end
+    self.tableView.registerClass(EntryCell, forCellReuseIdentifier:'Entry')
   end
+
   # テーブルの行数を返すメソッド
   def tableView(tableView, numberOfRowsInSection:section)
     @entries.count
   end
+
   # テーブルのセルを返すメソッド
   ENTRY_CELL_ID = 'Entry'
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
-    cell = tableView.dequeueReusableCellWithIdentifier(ENTRY_CELL_ID)
-    if cell.nil?
-      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:ENTRY_CELL_ID)
-    end
+    cell = tableView.dequeueReusableCellWithIdentifier(ENTRY_CELL_ID, forIndexPath:indexPath)
+
     entry = @entries[indexPath.row]
-    cell.textLabel.text = entry.title
-    cell.detailTextLabel.text = "#{entry.updated_at} by #{entry.username}"
+    cell.entry = entry
+    cell.setNeedsDisplay # 再描画させる
+
     cell
   end
+
   def tableView(tableView, didSelectRowAtIndexPath:indexPath)
     entry = @entries[indexPath.row]
-    # UIWebView を貼付けた ビューコントローラを作成
-    controller = UIViewController.new
-    webview = UIWebView.new
-    webview.frame = controller.view.frame # webview の表示サイズを調整
-    controller.view.addSubview(webview)
+    controller = EntryController.new
+    controller.entry = entry
     # 画面遷移
     navigationController.pushViewController(controller, animated:true)
-    # HTML を読み込む
-    webview.loadHTMLString(entry.body, baseURL:nil)
   end
 end
---
+++ app/views/entry_cell.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+class EntryCell < UITableViewCell
+  attr_accessor :entry
+
+  def initWithStyle(style, reuseIdentifier:reuseIdentifier)
+    # UITableViewCellStyleSubtitle スタイルを使う
+    super(UITableViewCellStyleSubtitle, reuseIdentifier:reuseIdentifier)
+  end
+
+  def drawRect(rect)
+    super
+    self.textLabel.text = @entry.title
+    self.detailTextLabel.text = "#{@entry.updated_at} by #{@entry.username}"
+  end
+end
